0510aef4fed7cb02507b9db9542d8ddc1d13c3e3
ptiforka <ptiforka@gmail.com>
2021-01-31 06:23:18 +0200

    add config.ru

--- main.rb
+++ main.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 
-get '/' do
-  'Hello world!'
-end
+class Main < Sinatra::Base
+  get '/' do
+    'Hello world!'
+  end
 
-get '/' do
-  "<p>This is <i>dynamic</i> content served via puma: #{rand(36**6).to_s(36)}"
+  get '/random' do
+    "<p>This is <i>dynamic</i> content served via puma: #{rand(36**6).to_s(36)}"
+  end
 end
